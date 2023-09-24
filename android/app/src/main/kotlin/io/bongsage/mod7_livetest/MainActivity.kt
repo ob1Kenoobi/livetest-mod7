74c474a6b22caecbbd1991866b22305dcc8b3ff7
Hasan Banna <hansolo132134@gmail.com>
2023-09-24 23:12:24 +0600

    initial commit.

---
+++ android/app/src/main/kotlin/io/bongsage/mod7_livetest/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.bongsage.mod7_livetest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CounterScreen(),
+    );
+  }
+}
+
+class CounterScreen extends StatefulWidget {
+  @override
+  _CounterScreenState createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int count = 0;
+
+  void _incrementCount() {
+    setState(() {
+      count++;
+      if (count >= 5) {
+        _showDialog();
+      }
+    });
+  }
+
+  void _decrementCount() {
+    setState(() {
+      if (count > 0) {
+        count--;
+      }
+    });
+  }
+
+  void _showDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Button pressed $count times"),
+          actions: <Widget>[
+            TextButton(
+              child: Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Counter App"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Count:",
+              style: TextStyle(fontSize: 24),
+            ),
+            Text(
+              "$count",
+              style: TextStyle(fontSize: 48),
+            ),
+            SizedBox(height: 20), // Add spacing here
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(width: 20), // Add spacing here
+                ElevatedButton(
+                  onPressed: _incrementCount,
+                  child: Text("+"),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: _decrementCount,
+                  child: Text("-"),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
